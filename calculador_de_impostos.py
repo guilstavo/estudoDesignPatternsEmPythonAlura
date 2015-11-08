2ffc2abc8fbdf318ed6f7616813fce1c57328583
Gustavo <gustavomartinis@gmail.com>
2015-11-08 17:17:33 -0200

    Códigos parecidos e o Template Method.

--- calculador_de_impostos.py
+++ calculador_de_impostos.py
@@ -1,4 +1,4 @@
-from impostos import ISS, ICMS
+from impostos import ISS, ICMS, ICPP, IKCV
 
 class Calculador_de_impostos(object):
 
@@ -11,11 +11,19 @@ class Calculador_de_impostos(object):
 
 if __name__ == '__main__':
 
-	from orcamento import Orcamento
+	from orcamento import Orcamento, Item
 
 	calculador = Calculador_de_impostos()
 
-	orcamento = Orcamento(500)
+	orcamento = Orcamento()
+	orcamento.adiciona_item(Item('Item - 1', 50))
+	orcamento.adiciona_item(Item('Item - 2', 200))
+	orcamento.adiciona_item(Item('Item - 3', 250))
 
+	print 'ISS e ICMS'
 	calculador.realiza_calculo(orcamento, ISS())
 	calculador.realiza_calculo(orcamento, ICMS())
+
+	print 'ICPP e IKCV'
+	calculador.realiza_calculo(orcamento, ICPP())
+	calculador.realiza_calculo(orcamento, IKCV())
